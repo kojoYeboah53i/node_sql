98bd97811c7d12a4a6e0770bdd3544d3540ef11e
Isaac Kojo Yeboah <yeboah.determined.isaac@gmail.com>
2023-11-02 04:01:50 -0700

    update

--- config/knexfile.js
+++ config/knexfile.js
@@ -58,6 +58,20 @@ module.exports = {
     }
   },
 
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: __dirname + "./migrations",
+    },
+    seeds: {
+      directory: __dirname + "./seeds",
+    },
+  },
+
 
 };
 
